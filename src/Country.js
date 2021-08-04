300e2526b076b2969423bde13eadc29bca3a0f5a
deejayemmydon <okinopatrick0@gmail.com>
2021-08-04 15:06:36 +0100

    second commit

--- src/Country.js
+++ src/Country.js
@@ -3,6 +3,13 @@ import './country.css'
 const url = 'https://restcountries.eu/rest/v2/all'
 function Country() {
     const [countries, setCountries] = useState([])
+    const [input, setInput] = useState('')
+    const [output, setOutput] = useState([])
+    const [modal, setModal] = useState(false)
+
+    const getModal = () => {
+        setModal(!modal)
+    }
 
 
     const fetchCountry = async () => {
@@ -12,19 +19,47 @@ function Country() {
     }
     useEffect(() => {
         fetchCountry()
-
+           
     }, [])
+ 
+    useEffect(() => {
+         setOutput([])
+        countries.filter(val=>{
+            if(val.name.toLowerCase().includes(input.toLowerCase())){
+                setOutput(output=>[...output, val])
+            }
+        })
+
+        
+         
+    }, [input, output])
 
     return (
         <div className="count">
             <h1>COUNTRY</h1>
+           <form action="">
+               <input type="text" placeholder='Search' onChange={e=>setInput(e.target.value)}/>
+           </form>
+           
             <div className='grid'>
 
-                {countries.map((country) => {
+                {output.map((country) => {
                     const { numericCode, name, flag, population, region, capital } = country
                     return <article key={numericCode}>
                         <div>
 
+                            <div className="particle  " >
+                                <img src={flag} alt="" />
+                                <h3>{name}</h3>
+                                <h4>Population: <span>{population}</span> </h4>
+                                <h4>Capital: <span>{capital}</span> </h4>
+                                <h4>Region: <span>{region} </span> </h4>
+                                <h4>Capital: <span>{capital} </span></h4>
+                                <button onClick={getModal}>Check</button>
+                            </div>
+                        </div>
+                                {modal &&(
+                                        <div className="pop-up  onClick={getModal}">
                             <div className="particle">
                                 <img src={flag} alt="" />
                                 <h3>{name}</h3>
@@ -34,6 +69,9 @@ function Country() {
                                 <h4>Capital: <span>{capital} </span></h4>
                             </div>
                         </div>
+                                )}
+                    
+                        
                     </article>
 
                 })}
